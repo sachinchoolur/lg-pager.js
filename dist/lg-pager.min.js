4eb670d873174f1ade424b54fa500561707fb433
sachin <sachi77n@gmail.com>
2016-08-01 09:12:37 +0530

    Dist

--- dist/lg-pager.min.js
+++ dist/lg-pager.min.js
@@ -1,5 +1,5 @@
 /**!
- * lg-pager.js | 0.0.1 | July 27th 2016
+ * lg-pager.js | 0.0.1 | August 1st 2016
  * http://sachinchoolur.github.io/lg-pager.js
  * Copyright (c) 2016 Sachin N; 
  * @license Apache 2.0 
